d5a292b3724c0b606256edba380ea7eb95c9a8a4
Robert Boyd III <rob@robertcboyd.com>
2019-11-21 14:05:04 -0500

    Better design

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const div0 = document.getElementById("data0");
-const div1 = document.getElementById("data1");
-const div2 = document.getElementById("data2");
+const div = document.getElementById("data");
 const button = document.getElementById("button");
 const searchBox = document.getElementById("search");
+
+const url0 = "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query="
+const url1 = "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/"
 
 button.addEventListener("click", handler);
 searchBox.addEventListener("keypress", e => {
@@ -11,13 +12,8 @@ searchBox.addEventListener("keypress", e => {
   }
 });
 
-const url0 = "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query="
-const url1 = "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/"
-
 function handler() {
-  div0.innerHTML = "Loading...";
-  div1.innerHTML = "";
-  div2.innerHTML = "";
+  div.innerHTML = "Loading...";
   const search = searchBox.value;
   fetch(url0 + search)
     .then(b => b.json())
@@ -27,15 +23,13 @@ function handler() {
         .then(b => b.json())
         .then(realData => {
           const today = realData.consolidated_weather[0];
-          div0.innerHTML = `Weather: ${today.weather_state_name}`;
-          div1.innerHTML = `High: ${toFarenheit(today.max_temp)}°F`;
-          div2.innerHTML = `Low: ${toFarenheit(today.min_temp)}°F`;
+          div.innerHTML = `Weather: ${today.weather_state_name}<br>
+                            High: ${toFarenheit(today.max_temp)}°F<br>
+                            Low: ${toFarenheit(today.min_temp)}°F`;
         });
     })
     .catch(() => {
-      div0.innerHTML = "Error!"
-      div1.innerHTML = ""
-      div2.innerHTML = ""
+      div.innerHTML = "Error!"
     });
 }
 
